b92d13e08dc2a11f893c8aad2440c5bad2142008
Haorui Wu <srxiaoj@gmail.com>
2016-04-22 15:05:10 -0400

    Add container with most water

---
+++ Two Pointers/src/ContainerWithMostWater.java
@@ -0,0 +1,33 @@
+/**
+ * Created by thanksgiving on 4/22/16.
+ */
+public class ContainerWithMostWater {
+    public static void main(String[] args) {
+        ContainerWithMostWater obj = new ContainerWithMostWater();
+        int[] h = new int[]{1, 2, 1, 4, 1, 2, 5, 7, 1, 4, 5, 2, 1};
+        int max = obj.maxArea(h);
+        System.out.println(max);
+
+    }
+
+    /**
+     * 每次最大面积为Math.min(height[i], height[j]) * (j - i))与当前最大面积比较
+     * 如果height[i] < height[j]， 则增加 i 去继续update最大面积，否则减少 j
+     * @param height
+     * @return
+     */
+    public int maxArea(int[] height) {
+        if (height == null) return 0;
+        int mx = 0;
+        int i = 0, j = height.length - 1;
+        while (i < j) {
+            mx = Math.max(mx, Math.min(height[i], height[j]) * (j - i));
+            if (height[i] < height[j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return mx;
+    }
+}
