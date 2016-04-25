6ca996046fe6d3055dfc31510dbd7a11f709d1ad
Haorui Wu <srxiaoj@gmail.com>
2016-04-25 11:20:51 -0400

    Modified kthSmallestBST

--- Binary Search/src/KthSmallest.java
+++ Binary Search/src/KthSmallest.java
@@ -1,10 +1,5 @@
-import java.util.Stack;
-
-
 public class KthSmallest {
-
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
         /*
          * http://www.geeksforgeeks.org/find-k-th-smallest-element-in-bst-order-statistics-in-bst/
          */
@@ -16,57 +11,68 @@ public class KthSmallest {
         node.left.right.left = new TreeNode(10);
         node.left.right.right = new TreeNode(14);
         System.out.println(kthSmallest(node, 3));
+
+        TreeNode node2 = new TreeNode(1);
+        node2.right = new TreeNode(2);
+        System.out.println(kthSmallest(node2, 2));
     }
+
     private static int number = 0;
     private static int count = 0;
-    public static int kthSmallest(TreeNode root, int k) {
-        //method 3: DFS recursive
-        // better keep these two variables in a wrapper class
-        
-            count = k;
-            helper(root);
-            return number;
 
+    /**
+     * 借用一个方法count一个节点下共有多少个节点
+     * 如果
+     */
+    public static int kthSmallest(TreeNode root, int k) {
+        //method 3: DFS in-order recursive
+        // better keep these two variables in a wrapper class
+//        count = k;
+//        helper(root);
+//        return number;
         /*
         //method 2: DFS in-order iterate
-        Stack<TreeNode> st = new Stack<>();
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
 
-        while (root != null) {
-            st.push(root);
-            root = root.left;
-        }
-
-        while (k != 0) {
-            TreeNode n = st.pop();
-            k--;
-            if (k == 0) return n.val;
-            TreeNode right = n.right;
-            while (right != null) {
-                st.push(right);
-                right = right.left;
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.peek();
+            if (node.left != null) {
+                stack.push(node.left);
+                node.left = null;
+            } else {
+                k--;
+                stack.pop();
+                if (k == 0) {
+                    return node.val;
+                }
+                if (node.right != null) {
+                    stack.push(node.right);
+                }
             }
         }
-        return -1; // never hit if k is valid
+        return -1;
         */
+
         //method 1: Binary Search (dfs): O(h), h is the height of tree
-        /*
-        int count = countNodes(root.left);
-        if (k <= count) {
+        int left = countNodes(root.left);
+        if (left > k - 1) {
             return kthSmallest(root.left, k);
-        } else if (k > count + 1) {
-            return kthSmallest(root.right, k-count-1);
+        } else if (left < k - 1) {
+            return kthSmallest(root.right, k - left - 1);
         } else {
             return root.val;
         }
-        */
     }
+
     private static int countNodes(TreeNode node) {
         if (node == null) {
             return 0;
         } else {
-            return countNodes(node.left)+ countNodes(node.right) + 1; 
+            return countNodes(node.left) + countNodes(node.right) + 1;
         }
     }
+
     public static void helper(TreeNode n) {
         if (n.left != null) helper(n.left);
         count--;
@@ -79,8 +85,11 @@ public class KthSmallest {
 }
 
 class TreeNode {
-     int val;
-     TreeNode left;
-     TreeNode right;
-     TreeNode(int x) { val = x; }
- }
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
