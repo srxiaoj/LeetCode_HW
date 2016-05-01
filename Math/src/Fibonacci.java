5cdd493f01c7eabb9f730a8be05a86bf726829b2
Haorui Wu <srxiaoj@gmail.com>
2016-04-30 23:08:20 -0400

    add fibonacci

--- Math/src/Fibonacci.java
+++ Math/src/Fibonacci.java
@@ -5,7 +5,7 @@ public class Fibonacci {
     public static void main(String[] args) {
         System.out.println(fibonacci(5));
         System.out.println(fibonacci(6));
-        System.out.println(fibonacci(7));
+        System.out.println(fibonacci(33));
     }
 
     public static int fibonacci(int n) {
@@ -13,14 +13,14 @@ public class Fibonacci {
         if (n == 1) return 1;
 
         // recursive
-//        return fibonacci(n - 1) + fibonacci(n - 2);
+        return fibonacci(n - 1) + fibonacci(n - 2);
 
         // iterative
-        int[] a = new int[n + 1];
-        a[1] = 1;
-        for (int i = 2; i < n + 1; i++) {
-            a[i] = a[i - 1] + a[i - 2];
-        }
-        return a[n];
+//        int[] a = new int[n + 1];
+//        a[1] = 1;
+//        for (int i = 2; i < n + 1; i++) {
+//            a[i] = a[i - 1] + a[i - 2];
+//        }
+//        return a[n];
     }
 }
