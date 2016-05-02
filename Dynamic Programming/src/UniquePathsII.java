e64ddf59f66ba002f58d244d729ff8f4785086a7
Haorui Wu <srxiaoj@gmail.com>
2016-05-02 00:52:49 -0400

    Modified UniquePathII

--- Dynamic Programming/src/UniquePathsII.java
+++ Dynamic Programming/src/UniquePathsII.java
@@ -2,14 +2,18 @@
 public class UniquePathsII {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int[][] A = {
-		              {1,0},
-		              {0,0},
+		              {0,0,0},
+                      {0,1,0},
+		              {0,0,0},
 		            };
 		int res = uniquePathsWithObstacles(A);
 		System.out.println("result is: " + res);
 	}
+
+    /**
+     * 如果有障碍，那么障碍位置的count为0
+     */
 	public static int uniquePathsWithObstacles(int[][] obstacleGrid) {
 		int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
@@ -20,41 +24,12 @@ public class UniquePathsII {
         else sum[1][1] = 1;
         for (int i = 1; i < m + 1; i++) {
             for (int j = 1; j < n + 1; j++) {
-                if (i != 1 || j != 1) {
+                if (!(i == 1 && j == 1)) {
                     if (obstacleGrid[i - 1][j - 1] == 1) sum[i][j] = 0;
                     else sum[i][j] = sum[i - 1][j] + sum[i][j - 1];
                 }
             }
         }
         return sum[m][n];
-
-        /*int[][] result = new int [m][n];//used to store the previous results
-        for(int i = 0; i < m; i++)
-        {
-            for(int j = 0; j < n; j++)
-            {
-                if(obstacleGrid[i][j] == 1)
-                {
-                	result[i][j] = 0;
-                }
-                else if(i == 0 || j == 0)
-                {
-                	if(i == 0 && j == 0)
-                	    result[i][j] = 1;
-                	else if(i == 0)
-                    {
-                    	result[i][j] = result[i][j-1];//check the cell on the left
-                    }
-                	else if(j == 0)
-                		result[i][j] = result[i-1][j];//check the cell on the top
-                }
-                else
-                {
-                	result[i][j] = result[i-1][j] + result[i][j-1];
-                }
-            }
-        }
-        return result[m-1][n-1];*/
     }
-
 }
