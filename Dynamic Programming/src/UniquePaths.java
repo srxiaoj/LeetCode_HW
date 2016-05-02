dcce2f5395b048cd76dbbfcb621044f702c00836
Haorui Wu <srxiaoj@gmail.com>
2016-05-02 00:46:57 -0400

    Modified UniquePath

--- Dynamic Programming/src/UniquePaths.java
+++ Dynamic Programming/src/UniquePaths.java
@@ -14,7 +14,7 @@ public class UniquePaths {
         sum[1][1] = 1;
         for (int i = 1; i < m + 1; i++) {
             for (int j = 1; j < n + 1; j++) {
-                if (i != 1 || j != 1)
+                if (!(i == 1 && j == 1))
                     sum[i][j] = sum[i - 1][j] + sum[i][j - 1];
             }
         }
