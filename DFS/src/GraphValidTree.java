bbfbdbd5e3c4680cd511a9a7580f52e0c752942a
Haorui Wu <srxiaoj@gmail.com>
2016-05-08 16:50:45 -0400

    comment PerfectSquares

--- DFS/src/GraphValidTree.java
+++ DFS/src/GraphValidTree.java
@@ -1,49 +1,72 @@
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.LinkedList;
 
 public class GraphValidTree {
-    private int V;
-    private LinkedList<Integer> adj[];
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        int[][] edges = {{0, 1}, {0, 2}, {0, 3}, {1, 4}};
-        int n = 5;
         GraphValidTree obj = new GraphValidTree();
 
+        int[][] edges = {{0, 1}, {0, 2}, {0, 3}, {1, 4}};
+        int n = 5;
         System.out.println(obj.validTree(n, edges));
     }
-    public void initGraphValidTree(int v) {
-        V = v;
-        adj = new LinkedList[v];
-        for (int i = 0; i < v; i++) {
-            adj[i] = new LinkedList<Integer>();
+
+    /**
+     * method1: union find
+     */
+    public boolean validTreei1(int n, int[][] edges) {
+        int[] root = new int[n];
+        for(int i = 0; i < n; i++)
+            root[i] = i;
+        for(int i = 0; i < edges.length; i++) {
+            int root1 = find(root, edges[i][0]);
+            int root2 = find(root, edges[i][1]);
+            if(root1 == root2)
+                return false;
+            root[root2] = root1;
         }
+        return edges.length == n - 1;
     }
+
+    private int find(int[] root, int e) {
+        if(root[e] == e)
+            return e;
+        else
+            return find(root, root[e]);
+    }
+
+
+    // method2: dfs
+    private LinkedList<Integer> adj[];
     private void addEdge(int w, int v) {
         adj[v].add(w);
         adj[w].add(v);
     }
+
     public boolean validTree(int n, int[][] edges) {
-        initGraphValidTree(n);
+        // create adjacentList
+        adj = new LinkedList[n];
+        for (int i = 0; i < n; i++) {
+            adj[i] = new LinkedList<>();
+        }
+
         for (int i = 0; i < edges.length; i++) {
             addEdge(edges[i][0], edges[i][1]);
         }
         if (edges.length != n - 1) return false;
-        if (isCyclic()) {
+        if (isCyclic(n)) {
             return false;
         } else {
             return true;
         }
     }
 
-    private Boolean isCyclic() {
-        Boolean visited[] = new Boolean[V];
-        for (int i = 0; i < V; i++) {
-            visited[i] = false;
-        }
+    private Boolean isCyclic(int n) {
+        Boolean visited[] = new Boolean[n];
+        Arrays.fill(visited, false);
 
-        for (int u = 0; u < V; u++) {
-            if (!visited[u] && isCyclicUntil(u, visited, -1)) {
+        for (int i = 0; i < n; i++) {
+            if (!visited[i] && isCyclicUntil(i, visited, -1)) {
                 return true;
             }
         }
@@ -52,11 +75,10 @@ public class GraphValidTree {
 
     private Boolean isCyclicUntil(int v, Boolean visited[], int parent) {
         visited[v] = true;
-        Integer i;
 
         Iterator<Integer> it = adj[v].iterator();
         while (it.hasNext()) {
-            i = it.next();
+            int i = it.next();
             if (!visited[i]) {
                 if (isCyclicUntil(i, visited, v)) {
                     return true;
--- Dynamic Programming/src/PerfectSquares.java
+++ Dynamic Programming/src/PerfectSquares.java
@@ -3,15 +3,20 @@ import java.util.Arrays;
 public class PerfectSquares {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
         System.out.println(numSquares(12));
     }
+
+    /**
+     * 核心步骤： dp[i] = Math.min(dp[i], dp[i - j * j] + 1)
+     * 如果i - j * j == 0, 则dp[i] = 0;
+     */
     public static int numSquares(int n) {
         int[] dp = new int[n+1];
         Arrays.fill(dp, Integer.MAX_VALUE);
         dp[0] = 0;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j*j <= i; j++) {
+                // if i - j * j == 0, then dp[i] = 0
                 dp[i] = Math.min(dp[i], dp[i - j * j] + 1); // 1 stands for dp[j *j] which should be 1
             }
         }
