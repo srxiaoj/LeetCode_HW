bbf93fd26436e04ee4f4f595a02606338b6e821a
Haorui Wu <srxiaoj@gmail.com>
2016-05-08 13:26:54 -0400

    comment SetMatrixZeroes

--- Array/src/SetMatrixZeroes.java
+++ Array/src/SetMatrixZeroes.java
@@ -4,10 +4,10 @@
 public class SetMatrixZeroes {
     public static void main(String[] args) {
         int[][] matrix = {
-                {1, 2, 0, 1, 1},
-                {1, 0, 1, 1, 1},
-                {0, 1, 0, 1, 0},
-                {7, 8, 4, 2, 4}
+                {1, 0, 1, 1, 0},
+                {1, 1, 1, 1, 1},
+                {1, 1, 0, 1, 1},
+                {7, 1, 4, 2, 4}
         };
         printArray(matrix);
         setZeroes(matrix);
@@ -15,64 +15,47 @@ public class SetMatrixZeroes {
 
     }
 
+    /**
+     * 从0行，1列开始遍历matrix，一旦发现matrix[i][j]为0
+     * 则将该行首以及该列首设置为0，再进行二次遍历
+     * 二次遍历时如果发现行首或者列首为0，则当前matrix[i][j]为0
+     * 二次遍历时应该逆序遍历，防止出现二次影响效果（改数为０是因为别的数为０而变化为０）
+     * 注意 j 的起点为1， j == 0的情况由col0IsZero来判断
+     */
     public static void setZeroes(int[][] matrix) {
-        int col0 = 1, rows = matrix.length, cols = matrix[0].length;
-
-        for (int i = 0; i < rows; i++) {
-            if (matrix[i][0] == 0) col0 = 0;
-            for (int j = 1; j < cols; j++)
-                if (matrix[i][j] == 0)
-                    matrix[i][0] = matrix[0][j] = 0;
-        }
-
-        for (int i = rows - 1; i >= 0; i--) {
-            for (int j = cols - 1; j >= 1; j--)
-                if (matrix[i][0] == 0 || matrix[0][j] == 0)
-                    matrix[i][j] = 0;
-            if (col0 == 0) matrix[i][0] = 0;
-        }
-
-
-        /*if (matrix == null) return;
-        int row = matrix.length;
-        if (row == 0) return;
-        int col = matrix[0].length;
-        if (col == 0) return;
-
-        boolean[] zeroRow = new boolean[row];
-        boolean[] zeroCol = new boolean[col];
-
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean col0IsZero = false;
+        // set the head of that row and that col as 0 if one element in that row/col is 0
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) col0IsZero = true;
+            for (int j = 1; j < n; j++) {
                 if (matrix[i][j] == 0) {
-                    zeroRow[i] = true;
-                    zeroCol[j] = true;
+                    matrix[i][0] = matrix[0][j] = 0;
                 }
             }
         }
 
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (zeroRow[i]) {
-                    matrix[i][j] = 0;
-                }
-                if (zeroCol[j]) {
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 1; j--) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
                 }
             }
-        }*/
+            if (col0IsZero) {
+                matrix[i][0] = 0;
+            }
+        }
 
     }
+
     /**
      * print 2D array.
-     * @param A array
      */
-    public static void printArray(int[][] A)
-    {
-        for(int i = 0; i < A.length; i++)
-        {
+    public static void printArray(int[][] A) {
+        for (int i = 0; i < A.length; i++) {
             for (int j = 0; j < A[i].length; j++) {
-                if(j != A[i].length-1) {
+                if (j != A[i].length - 1) {
                     System.out.print(A[i][j] + ", ");
                 } else
                     System.out.print(A[i][j]);
