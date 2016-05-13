1fad6d5918fb7e27b6fe72ab7cf0e149b45ba62c
Haorui Wu <srxiaoj@gmail.com>
2016-05-13 00:53:12 -0400

    Comment wordBreak

--- Dynamic Programming/src/WordBreak.java
+++ Dynamic Programming/src/WordBreak.java
@@ -13,13 +13,15 @@ public class WordBreak {
         System.out.println(obj.wordBreak("cars", dict));
     }
 
+    /**
+     * 如果s.substring(0,j)是wordbreakable, 而且s.substring(j,i)在wordDict中，则s.substring(0, i)是wordbreakable
+     */
     public boolean wordBreak(String s, Set<String> wordDict) {
-        if (s == null && wordDict == null)
-            return true;
-        if (s == null || wordDict == null)
-            return false;
+        if (s == null && wordDict == null) return true;
+        if (s == null || wordDict == null) return false;
         //dp[i] represents if s.substring(0, i) is wordbreakable.
-        boolean[] dp = new boolean[s.length()+1];
+        boolean[] dp = new boolean[s.length() + 1];
+        Arrays.fill(dp, false);
         dp[0] = true;
         for (int i = 1; i <= s.length(); i++) {
             for (int j = 0; j < i; j++) {
