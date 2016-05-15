d8068b1a61f133f2abd74394f06232dde4494a66
Haorui Wu <srxiaoj@gmail.com>
2016-05-14 20:07:37 -0400

    Comment EvaluateReversePolishNotation

--- String/src/EvaluateReversePolishNotation.java
+++ String/src/EvaluateReversePolishNotation.java
@@ -1,8 +1,8 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Stack;
 
-/**
- * Created by thanksgiving on 1/1/16.
- */
 public class EvaluateReversePolishNotation {
     public static void main(String[] args) {
         EvaluateReversePolishNotation obj = new EvaluateReversePolishNotation();
@@ -10,36 +10,37 @@ public class EvaluateReversePolishNotation {
         String[] tokens = {"3","-4","+"};
         System.out.println(obj.evalRPN(tokens));
     }
+
+    /**
+     * 借用stack的特点，将非符号的String放入stack，然后在碰到符号的时候取出两个数字进行操作
+     * 再将结果放入stack
+     */
     public int evalRPN(String[] tokens) {
-        if (tokens.length == 0 || tokens == null) return 0;
-        if (tokens.length == 1) return Integer.parseInt(tokens[0]);
-        Stack<String> stack = new Stack<>();
-        int i = 0;
-        while (i < tokens.length) {
-            while (Character.isDigit(tokens[i].charAt(tokens[i].length() - 1))) {
-                stack.add(tokens[i]);
-                i++;
+        List<String> op = new ArrayList<>(Arrays.asList("+", "-", "*", "/"));
+        if (tokens == null) return 0;
+        Stack<Integer> stack = new Stack<>();
+        for (String s : tokens) {
+            if (!op.contains(s)) {
+                stack.add(Integer.parseInt(s));
+            } else {
+                int b = stack.pop();
+                int a = stack.pop();
+                int sub = operate(a, b, s);
+                stack.add(sub);
             }
-            int operant = Integer.parseInt(stack.pop());
-            int operand = Integer.parseInt(stack.pop());
-            int res = compute(operand, tokens[i], operant);
-            stack.add(String.valueOf(res));
-            i++;
         }
-        return Integer.parseInt(stack.lastElement());
+        return stack.pop();
     }
-    private int compute(int operand, String op, int operant) {
-        switch (op) {
-            case "+":
-                return operand + operant;
-            case "-":
-                return operand - operant;
-            case "*":
-                return operand * operant;
-            case "/":
-                return operand / operant;
-            default:
-                return 0;
+
+    private int operate(int a, int b, String s) {
+        if (s.equals("+")) {
+            return a + b;
+        } else if (s.equals("-")) {
+            return a - b;
+        } else if (s.equals("*")) {
+            return a * b;
+        } else {
+            return a / b;
         }
     }
 }
