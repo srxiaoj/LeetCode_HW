72509adedd4b7b59bc948eeb933b6c4184d15952
Haorui Wu <srxiaoj@gmail.com>
2016-06-05 13:11:04 -0400

    Comment BitToInt

--- Bit Manipulation/src/BitToInt.java
+++ Bit Manipulation/src/BitToInt.java
@@ -13,15 +13,32 @@ public class BitToInt {
         System.out.println(bitToInt(bit4));
         String bit5 = "0111";
         System.out.println(bitToInt(bit5));
+        String bit6 = "11111111111111111111111111111111";
+        System.out.println(bitToInt(bit6));
+        String bit7 = "11111111111111111111111111111110";
+        System.out.println(bitToInt(bit7));
     }
 
+    /**
+     * 前31位每位按照正常算法计算
+     * 到32位如果为1则为 -2147483648 + 前31位结果
+     */
     public static int bitToInt(String bit) {
         int n = bit.length();
-        int res = 0;
-        for (int i = 0; i < n; i++) {
-            int digit = bit.charAt(i) - '0';
-            res += (int) Math.pow(2, n - i - 1) * digit;
+        if (n > 32) return 0;
+        if (n < 32) {
+            int res = 0;
+            for (int i = 0; i < n; i++) {
+                int digit = bit.charAt(i) - '0';
+                res += (int) Math.pow(2, n - i - 1) * digit;
+            }
+            return res;
+        } else {
+            if (bit.charAt(0) == '0') {
+                return bitToInt(bit.substring(1));
+            } else {
+                return Integer.MIN_VALUE + bitToInt(bit.substring(1));
+            }
         }
-        return res;
     }
 }
