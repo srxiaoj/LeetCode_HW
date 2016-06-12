ab5cf96b842c311aafc903d9620cdd0d4509e587
Haorui Wu <srxiaoj@gmail.com>
2016-06-12 15:09:19 -0400

    Add LongestSubstringwithAtMostKDistinctCharacters

---
+++ Two Pointers/src/LongestSubstringwithAtMostKDistinctCharacters.java
@@ -0,0 +1,32 @@
+/**
+ * Created by thanksgiving on 6/12/16.
+ */
+public class LongestSubstringwithAtMostKDistinctCharacters {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 搞一个map，256位，在每个character位统计character的个数，并且搞个num统计distinct number数量
+     * 如果distinct number数量比k大，则不断从i开始减去开头的字符直到 num <= k为止
+     */
+    public int lengthOfLongestSubstringKDistinct(String s, int k) {
+        int[] map = new int[256];
+        int num = 0;
+        int res = 0;
+        int i = 0, j = 0;
+        for (j = 0; j < s.length(); j++) {
+            while (num > k && i < j) {
+                map[s.charAt(i)]--;
+                if (map[s.charAt(i)] == 0) num--;
+                i++;
+            }
+            if (map[s.charAt(j)] == 0) num++;
+            map[s.charAt(j)]++;
+
+            if (num <= k)
+                res = Math.max(res, j - i + 1);
+        }
+        return res;
+    }
+}
--- Two Pointers/src/LongestSubstringwithAtMostTwoDistinctCharacters.java
+++ Two Pointers/src/LongestSubstringwithAtMostTwoDistinctCharacters.java
@@ -7,9 +7,38 @@ public class LongestSubstringwithAtMostTwoDistinctCharacters {
     public static void main(String[] args) {
         LongestSubstringwithAtMostTwoDistinctCharacters obj = new LongestSubstringwithAtMostTwoDistinctCharacters();
         String s = "cabcccacacccbbcbb";
+        System.out.println(obj.lengthOfLongestSubstringTwoDistinct(s, 2));
         System.out.println(obj.lengthOfLongestSubstringTwoDistinct(s));
     }
 
+    /**
+     * 方法1：搞一个map，256位，在每个character位统计character的个数，并且搞个num统计distinct number数量
+     * 如果distinct number数量比k大，则不断从i开始减去开头的字符直到 num <= k为止
+     */
+    public int lengthOfLongestSubstringTwoDistinct(String s, int k) {
+        int[] map = new int[256];
+        int num = 0;
+        int res = 0;
+        int i = 0, j = 0;
+        for (j = 0; j < s.length(); j++) {
+            while (num > k && i < j) {
+                map[s.charAt(i)]--;
+                if (map[s.charAt(i)] == 0) num--;
+                i++;
+            }
+            if (map[s.charAt(j)] == 0) num++;
+            map[s.charAt(j)]++;
+
+            if (num <= k)
+                res = Math.max(res, j - i + 1);
+        }
+        return res;
+    }
+
+
+    /**
+     * 方法2
+     */
     public int lengthOfLongestSubstringTwoDistinct(String s) {
         if (s == null || s.length() == 0) return 0;
         int i = 0, j = 1;
