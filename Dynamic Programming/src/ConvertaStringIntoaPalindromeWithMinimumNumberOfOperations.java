22e78eaba9b2929301bd01e3fd65407084006b86
Haorui Wu <srxiaoj@gmail.com>
2016-06-13 13:09:59 -0400

    Add ShortestPalindrome

---
+++ Dynamic Programming/src/ConvertaStringIntoaPalindromeWithMinimumNumberOfOperations.java
@@ -0,0 +1,13 @@
+/**
+ * Created by thanksgiving on 6/13/16.
+ */
+public class ConvertaStringIntoaPalindromeWithMinimumNumberOfOperations {
+    public static void main(String[] args) {
+        System.out.println(convertNum("mohammadsajjadhossain"));
+    }
+
+    public static int convertNum(String s) {
+
+        return 0;
+    }
+}
