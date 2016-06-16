d0f766f4faee43c81a1c3613bc055d1da5106928
Haorui Wu <srxiaoj@gmail.com>
2016-06-16 12:27:04 -0400

    Comment Heapsort

--- Sort/src/HeapSort.java
+++ Sort/src/HeapSort.java
@@ -6,36 +6,44 @@ public class HeapSort {
     private static int N;
 
     /* Sort Function */
-    public static void sort(int[] arr) {
-        heapify(arr);
+    public static void sort(int[] num) {
+        N = num.length - 1;
+
+        // 将heap初始化为所有的parent都比children节点的值要大
+        initHeapify(num);
+        // 每次heapfy都将第一个数（当前的最大数）与最后一个位置交换，并且不断减小最后一个位置
+        // 从而得到升序排序
         for (int i = N; i > 0; i--) {
-            swap(arr, 0, i);
+            swap(num, 0, i);
             N = N - 1;
-            maxheap(arr, 0);
+            reHeapify(num, 0);
         }
     }
 
-    /* Function to build a heap */
-    public static void heapify(int[] arr) {
-        N = arr.length - 1;
+    /**
+     * 将heap初始化为所有的parent都比children节点的值要大
+     */
+    public static void initHeapify(int[] num) {
         for (int i = N / 2; i >= 0; i--) {
-            maxheap(arr, i);
+            reHeapify(num, i);
         }
     }
 
-    /* Function to swap largest element in heap */
-    public static void maxheap(int[] arr, int i) {
+    /**
+     * 从底至上，如果左右节点的值有比parent值大的，那么就swap，并且重新reHeapify那个max节点
+     */
+    public static void reHeapify(int[] num, int i) {
         int left = 2 * i;
         int right = 2 * i + 1;
         int max = i;
-        if (left <= N && arr[left] > arr[i])
+        if (left <= N && num[left] > num[i])
             max = left;
-        if (right <= N && arr[right] > arr[max])
+        if (right <= N && num[right] > num[max])
             max = right;
 
         if (max != i) {
-            swap(arr, i, max);
-            maxheap(arr, max);
+            swap(num, i, max);
+            reHeapify(num, max);
         }
     }
 
