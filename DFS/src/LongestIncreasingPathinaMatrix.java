b4a456dcf0219fdf3576f1f1ec2b4410e8e1b29a
Haorui Wu <srxiaoj@gmail.com>
2016-06-20 21:32:19 -0400

    Modified longestIncreasingPath

--- DFS/src/LongestIncreasingPathinaMatrix.java
+++ DFS/src/LongestIncreasingPathinaMatrix.java
@@ -21,26 +21,27 @@ public class LongestIncreasingPathinaMatrix {
         int m  = matrix.length;
         if (m == 0) return 0;
         int n = matrix[0].length;
-         int[] array = new int[1];
+        int[] array = new int[1];
+        int[][] len = new int[m][n];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                dfs(matrix, i, j, Integer.MIN_VALUE, 0, array);
+                dfs(matrix, i, j, Integer.MIN_VALUE, 0, array, len);
             }
         }
         return array[0];
     }
 
-    private void dfs(int[][] matrix, int i, int j, int lastValue, int res, int[] array) {
-        if (i < 0 || i >= matrix.length || j < 0 || j >= matrix[0].length
-                || matrix[i][j] <= lastValue)
-            return;
+    private void dfs(int[][] matrix, int i, int j, int lastValue, int res, int[] array, int[][] len) {
+        if (i < 0 || i >= matrix.length || j < 0 || j >= matrix[0].length || matrix[i][j] <= lastValue) return;
         res++;
-        if (res > array[0]) {
-            array[0] = res;
+        // 利用这个条件无意义dfs
+        if (res > len[i][j]) {
+            len[i][j] = res;
+            array[0] = Math.max(array[0], res);
+            dfs(matrix, i + 1, j, matrix[i][j], res, array, len);
+            dfs(matrix, i - 1, j, matrix[i][j], res, array, len);
+            dfs(matrix, i, j + 1, matrix[i][j], res, array, len);
+            dfs(matrix, i, j - 1, matrix[i][j], res, array, len);
         }
-        dfs(matrix, i + 1, j, matrix[i][j], res, array);
-        dfs(matrix, i - 1, j, matrix[i][j], res, array);
-        dfs(matrix, i, j + 1, matrix[i][j], res, array);
-        dfs(matrix, i, j - 1, matrix[i][j], res, array);
     }
 }
