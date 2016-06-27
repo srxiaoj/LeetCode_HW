b49267dc3be1b4a88c2736cf96ea03f389634ea3
Haorui Wu <srxiaoj@gmail.com>
2016-06-27 16:22:15 -0400

    Modified FlipGameII

--- Backtracking/src/FlipGameII.java
+++ Backtracking/src/FlipGameII.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by thanksgiving on 4/23/16.
@@ -23,6 +25,34 @@ public class FlipGameII {
                 }
             }
         }
+        return false;
+    }
+
+
+    /**
+     * 使用map存下每个string的结果，速度更快
+     */
+    public boolean canWin2(String s) {
+        Map<String, Boolean> map = new HashMap<>();
+        return helper(s, map);
+    }
+
+    private boolean helper(String s, Map<String, Boolean> map) {
+        if (map.containsKey(s)) {
+            return map.get(s);
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.startsWith("++", i)) {
+                String temp = s.substring(0, i) + "--" + s.substring(i + 2);
+                if (helper(temp, map)) {
+                    continue;
+                }
+                map.put(s, true);
+                return true;
+            }
+        }
+        map.put(s, false);
         return false;
     }
 }
