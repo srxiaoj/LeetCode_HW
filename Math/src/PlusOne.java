a164cbfd25cd1571fd761f139d9d26d91754d46f
Haorui Wu <srxiaoj@gmail.com>
2016-07-13 22:13:09 -0400

    Add ClosestNumberInSortArray

--- Math/src/PlusOne.java
+++ Math/src/PlusOne.java
@@ -3,20 +3,30 @@
  */
 public class PlusOne {
     public static void main(String[] args) {
-        int[] digits = {9, 9};
-        System.out.println(plusOne(digits)[0]);
+        int[] digits = {9, 9, 9, 9, 9};
+        int[] res = plusOne(digits);
+        for (int i = 0; i < res.length; i++) {
+            System.out.print(res[i]);
+        }
     }
 
+    /**
+     * plus any val
+     */
     public static int[] plusOne(int[] digits) {
-        int sum = 1;
+        int val = 8;
         for (int i = digits.length - 1; i >= 0; i--) {
-            digits[i] += sum;
+            digits[i] += val;
             if (digits[i] <= 9) // early return
                 return digits;
-            digits[i] = 0;
+            val = digits[i] / 10;
+            digits[i] = digits[i] % 10;
         }
         int[] ret = new int[digits.length + 1];
         ret[0] = 1;
+        for (int i = 1; i < ret.length; i++) {
+            ret[i] = digits[i - 1];
+        }
         return ret;
     }
 }
---
+++ Sort/src/ClosestNumberInSortArray.java
@@ -0,0 +1,29 @@
+/**
+ * Created by thanksgiving on 7/13/16.
+ */
+public class ClosestNumberInSortArray {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 3, 3, 3, 7, 8, 10, 100};
+        System.out.println(findClosestVal(a, 6));
+    }
+
+    public static int findClosestVal(int[] a, int target) {
+        int l = 0, r = a.length - 1;
+        while (l + 1 < r) {
+            int mid = l + (r - l) / 2;
+            if (a[mid] == target) {
+                return target;
+            } else if (a[mid] < target) {
+                l = mid;
+            } else {
+                r = mid;
+            }
+        }
+        System.out.println("l: " + l + " r: " + r);
+        if (Math.abs(target - a[l]) <= Math.abs(a[r] - target)) {
+            return a[l];
+        } else {
+            return a[r];
+        }
+    }
+}
