4828cb771e1eba2d6bd87cb7067f6bdc44cd188f
Haorui Wu <srxiaoj@gmail.com>
2016-07-23 15:11:08 -0400

    Add MeetingRoomIII

---
+++ Snapchat/src/MeetingRoomIII.java
@@ -0,0 +1,125 @@
+import java.util.*;
+
+/**
+ * Created by thanksgiving on 7/23/16.
+ */
+public class MeetingRoomIII {
+    public static void main(String[] args) {
+        Interval[] test = new Interval[3];
+        test[0] = new Interval(0, 30);
+        test[1] = new Interval(5, 10);
+        test[2] = new Interval(15, 20);
+        System.out.println(getRoomSchedule(test));
+
+        Interval[] test2 = new Interval[3];
+        test2[0] = new Interval(9, 10);
+        test2[1] = new Interval(4, 9);
+        test2[2] = new Interval(4, 17);
+        System.out.println(getRoomSchedule(test2));
+
+        Interval[] test3 = new Interval[4];
+        test3[0] = new Interval(1, 3);
+        test3[1] = new Interval(2, 9);
+        test3[2] = new Interval(12, 16);
+        test3[3] = new Interval(16, 20);
+        System.out.println(getRoomSchedule(test3));
+    }
+
+    public static List<Integer> getRoomSchedule(Interval[] intervals) {
+        List<Integer> res = new ArrayList<>();
+        if (intervals == null || intervals.length == 0) return res;
+        int n = intervals.length;
+        int[] output = new int[n];
+
+        Pair[] pairs = new Pair[n];
+        for (int i = 0; i < n; i++) {
+            pairs[i] = new Pair(intervals[i], i);
+        }
+        Arrays.sort(pairs, new PairComparator());
+        PriorityQueue<Room> pq = new PriorityQueue<>(new RoomComparator());
+        int numRooms = 1;
+        pq.offer(new Room(numRooms, pairs[0].interval.end));
+        output[pairs[0].originIndex] = numRooms;
+        for (int i = 1; i < pairs.length; i++) {
+            Room lastRoom = pq.peek();
+            if (pairs[i].interval.start < lastRoom.endTime) {
+                numRooms++;
+                output[pairs[i].originIndex] = numRooms;
+            } else {
+                pq.poll();
+                output[pairs[i].originIndex] = lastRoom.roomIndex;
+            }
+            pq.offer(new Room(numRooms, pairs[i].interval.end));
+        }
+        System.out.println(pq);
+
+
+        // output res
+        for (int i = 0; i < n; i++) {
+            res.add(output[i]);
+        }
+        return res;
+    }
+
+
+    private static class Pair {
+        Interval interval;
+        int originIndex;
+
+        public Pair(Interval l, int i) {
+            this.interval = l;
+            this.originIndex = i;
+        }
+
+        public String toString() {
+            return originIndex + ":" + interval.toString();
+        }
+    }
+
+    private static class Room {
+        int roomIndex;
+        int endTime;
+
+        public Room(int roomIndex, int endTime) {
+            this.roomIndex = roomIndex;
+            this.endTime = endTime;
+        }
+
+        public String toString() {
+        return "Room " + roomIndex + ", endTime: " + endTime;
+        }
+    }
+
+    private static class Interval {
+        int start;
+        int end;
+
+        Interval() {
+            start = 0;
+            end = 0;
+        }
+
+        Interval(int s, int e) {
+            start = s;
+            end = e;
+        }
+
+        public String toString() {
+            return "[" + start + "," + end + "]";
+        }
+    }
+
+    private static class PairComparator implements Comparator<Pair> {
+        @Override
+        public int compare(Pair p1, Pair p2) {
+            return p1.interval.start - p2.interval.start;
+        }
+    }
+
+    private static class RoomComparator implements Comparator<Room> {
+        @Override
+        public int compare(Room r1, Room r2) {
+            return r1.endTime - r2.endTime;
+        }
+    }
+}
--- Sort/src/MeetingRoomsII.java
+++ Sort/src/MeetingRoomsII.java
@@ -1,37 +1,38 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 /**
  * Created by thanksgiving on 1/9/16.
  */
 public class MeetingRoomsII {
     public static void main(String[] args) {
-        MeetingRoomsII obj = new MeetingRoomsII();
         Interval[] test = new Interval[3];
         test[0] = new Interval(0, 30);
         test[1] = new Interval(5, 10);
         test[2] = new Interval(15, 20);
-        System.out.println(obj.minMeetingRooms(test));
+//        System.out.println(minMeetingRooms(test));
 
         Interval[] test2 = new Interval[3];
         test2[0] = new Interval(9, 10);
         test2[1] = new Interval(4, 9);
         test2[2] = new Interval(4, 17);
-        System.out.println(obj.minMeetingRooms(test2));
+//        System.out.println(minMeetingRooms(test2));
 
         Interval[] test3 = new Interval[4];
         test3[0] = new Interval(1, 3);
         test3[1] = new Interval(2, 9);
         test3[2] = new Interval(12, 16);
         test3[3] = new Interval(16, 20);
-        System.out.println(obj.minMeetingRooms(test3));
+//        System.out.println(minMeetingRooms(test3));
     }
 
-    public int minMeetingRooms(Interval[] intervals) {
+    public static int minMeetingRooms(Interval[] intervals) {
         /**
          * 方法1： 将start与end分别提取出来，变成两个array，进行排序
          *        对比两个array有多少overlap的区间, 思维难度高, nlog(n)
          */
-        if (intervals == null || intervals.length == 0) {
+       /* if (intervals == null || intervals.length == 0) {
             return 0;
         }
 
@@ -66,25 +67,19 @@ public class MeetingRoomsII {
         }
         System.out.println(activeMeetings);
         return numMeetingRooms;
-
+*/
 
         /**
          * 方法2: nlog(n)
          */
-      /*  if (intervals == null || intervals.length == 0) {
+        if (intervals == null || intervals.length == 0) {
             return 0;
         }
-
         Arrays.sort(intervals, new IntervalComparator());
-
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         int numRooms = 1;
-
         pq.offer(intervals[0].end);
-
         for (int i = 1; i < intervals.length; i++) {
-            System.out.println(pq);
-            System.out.println(numRooms);
             if (intervals[i].start < pq.peek()) {
                 numRooms++;
                 pq.offer(intervals[i].end);
@@ -92,24 +87,49 @@ public class MeetingRoomsII {
                 pq.poll();
                 pq.offer(intervals[i].end);
             }
-            System.out.println(pq);
         }
-
-        return numRooms;*/
-
+        // return pq.size() // priorityQueue的size也就是每个meeting room的所需end时间
+        return numRooms;
     }
 
-    public class IntervalComparator implements Comparator<Interval> {
+    public static class IntervalComparator implements Comparator<Interval> {
         @Override
-        public int compare(Interval a, Interval b) {
-            return a.start - b.start;
+        public int compare(Interval l1, Interval l2) {
+            if (l1.start == l2.start) {
+                return l1.end - l2.end;
+            } else {
+                return l1.start - l2.start;
+            }
         }
     }
 
     private static class Interval {
         int start;
         int end;
-        Interval() { start = 0; end = 0; }
-        Interval(int s, int e) { start = s; end = e; }
+
+        Interval() {
+            start = 0;
+            end = 0;
+        }
+
+        Interval(int s, int e) {
+            start = s;
+            end = e;
+        }
+
+        public String toString() {
+            return "[" + start + "," + end + "]";
+        }
+    }
+
+    public static void printArray(int[] A) {
+        System.out.print("[");
+        for (int i = 0; i < A.length; i++) {
+            if (i != A.length - 1)
+                System.out.print(A[i] + ", ");
+            else
+                System.out.print(A[i]);
+        }
+        System.out.println("]");
     }
 }
