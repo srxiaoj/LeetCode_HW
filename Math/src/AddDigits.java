6c10d5e196eb9cab784df3aff9b7d9f0f699ac69
Haorui Wu <srxiaoj@gmail.com>
2016-07-29 22:29:00 -0400

    Modified AddDigits

--- Math/src/AddDigits.java
+++ Math/src/AddDigits.java
@@ -7,7 +7,19 @@ public class AddDigits {
     }
 
     public static int addDigits(int num) {
-        return 1 + (num - 1) % 9;
+        int res = 0;
+        while (num > 0) {
+            res += num % 10;
+            num /= 10;
+            if (num == 0 && res > 9) {
+                num = res;
+                res = 0;
+            }
+        }
+        return res;
+
+
+//        return 1 + (num - 1) % 9;
         /*
         //method 1
         int res = 0;
