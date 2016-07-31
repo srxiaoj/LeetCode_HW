e789b3cc353497f8e838c19374ff4b653998707c
Haorui Wu <srxiaoj@gmail.com>
2016-07-31 16:22:28 -0400

    Modified MoveZeros

--- Two Pointers/src/MoveZeroes.java
+++ Two Pointers/src/MoveZeroes.java
@@ -2,16 +2,31 @@
 public class MoveZeroes {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
         int[] test = {2, 1, 0, 3, 12};
         moveZeroes(test);
         printArray(test);
     }
     public static void moveZeroes(int[] nums) {
+        int i = 0, id1 = 0;
+        while (i < nums.length) {
+            if (nums[i] != 0) {
+                nums[id1] = nums[i];
+                id1++;
+            }
+            i++;
+        }
+
+        i = id1;
+        while (i < nums.length) {
+            nums[i] = 0;
+            i++;
+        }
+
+
         /**
          * insertion sort
          */
-        for (int i = 1; i < nums.length; i++) {
+        /*for (int i = 1; i < nums.length; i++) {
             for (int j = i; j > 0; j--) {
                 //core step, treat 0 as the biggest number
                 //only swap 0, but not the others
@@ -19,7 +34,7 @@ public class MoveZeroes {
                     swap(nums, j, j - 1);
                 }
             }
-        }
+        }*/
     }
     private static void swap(int[] nums, int i, int j) {
         int tmp = nums[i];
