8419b63460e7058b453620eb53a3169676b9c300
Haorui Wu <srxiaoj@gmail.com>
2016-07-31 21:02:58 -0400

    Modified CopyListwithRandomPointer

--- LinkedList/src/CopyListwithRandomPointer.java
+++ LinkedList/src/CopyListwithRandomPointer.java
@@ -7,43 +7,38 @@ public class CopyListwithRandomPointer {
     }
 
     public RandomListNode copyRandomList(RandomListNode head) {
-        if (head == null) {
-            return null;
-        }
-
+        if (head == null) return null;
         RandomListNode cur = head;
+        // duplicate each node
         while (cur != null) {
-            RandomListNode clone = new RandomListNode(cur.label);
             RandomListNode nxt = cur.next;
-            cur.next = clone;
-            clone.next = nxt;
+            cur.next = new RandomListNode(cur.label);
+            cur.next.next = nxt;
             cur = nxt;
         }
-
+        // copy random pointer
         cur = head;
         while (cur != null) {
-            RandomListNode nxt = cur.next;
-            if (cur.random != null)
-                nxt.random = cur.random.next;
-            else
-                nxt.random = null;
+            if (cur.random == null) {
+                cur.next.random = null;
+            } else {
+                cur.next.random = cur.random.next;
+            }
             cur = cur.next.next;
         }
-
-        //detach list
+        // extract duplicated list and recover original list
         cur = head;
-        RandomListNode res = head.next;
-        RandomListNode resCur = head.next;
+        RandomListNode res = cur.next;
+        RandomListNode copy = res;
         while (cur != null) {
             cur.next = cur.next.next;
-            if (resCur.next == null) {
-                break;
-            }
-            resCur.next = resCur.next.next;
+            if (copy.next == null) break;
 
-            resCur = resCur.next;
+            copy.next = copy.next.next;
             cur = cur.next;
+            copy = copy.next;
         }
+
         return res;
     }
 
