63fa941154dc3f4b1eb7c588fb61eafa1c594525
Haorui Wu <srxiaoj@gmail.com>
2016-08-02 13:41:48 -0400

    Modified firstMissingPositive

--- Array/src/FirstMissingPositive.java
+++ Array/src/FirstMissingPositive.java
@@ -4,10 +4,16 @@
 public class FirstMissingPositive {
     public static void main(String[] args) {
         FirstMissingPositive obj = new FirstMissingPositive();
-        int[] a = {1, 2, 3, 4, 4, 9};
-        System.out.println(obj.firstMissingPositive(a));
+//        System.out.println(obj.firstMissingPositive(new int[]{1, 2, 3, 4, 4, 9}));
+        System.out.println(obj.firstMissingPositive(new int[]{1, 2, 0}));
+        System.out.println(obj.firstMissingPositive(new int[]{3, 4, -1, 1}));
+        System.out.println(obj.firstMissingPositive(new int[]{1}));
+        System.out.println(obj.firstMissingPositive(new int[]{0}));
     }
 
+    /**
+     * nums[nums[i] - 1] != nums[i]这个条件很重要，防止无限循环swap
+     */
     public int firstMissingPositive(int[] nums) {
         if (nums == null || nums.length == 0) return 1;
         int n = nums.length;
@@ -29,5 +35,16 @@ public class FirstMissingPositive {
         int temp = a[i];
         a[i] = a[j];
         a[j] = temp;
+    }
+
+    public static void printArray(int[] A) {
+        System.out.print("[");
+        for (int i = 0; i < A.length; i++) {
+            if (i != A.length - 1)
+                System.out.print(A[i] + ", ");
+            else
+                System.out.print(A[i]);
+        }
+        System.out.println("]");
     }
 }
