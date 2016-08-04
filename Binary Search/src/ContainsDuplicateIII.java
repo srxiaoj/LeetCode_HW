83acf01e13bd9bcf37fb7a5114f22c658316e5d0
Haorui Wu <srxiaoj@gmail.com>
2016-08-03 22:22:16 -0400

    Add ContainsDuplicateIII

--- Binary Search/src/ContainsDuplicateIII.java
+++ Binary Search/src/ContainsDuplicateIII.java
@@ -1,52 +1,34 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.TreeMap;
 
 
 public class ContainsDuplicateIII {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        int[] test = {-1,2147483647};
-        int[] test2 = {1,4,6,8};
-        int[] test3 = {2,1};
-        System.out.println("The array contains duplicate: " + containsNearbyAlmostDuplicate(test, 1, 2147483647));
-        System.out.println("The array contains duplicate: " + containsNearbyAlmostDuplicate(test2, 1, 5));
-        System.out.println("The array contains duplicate: " + containsNearbyAlmostDuplicate(test3, 1, 1));
+        int[] test = {-1, 2147483647};
+        int[] test2 = {1, 4, 6, 8};
+        int[] test3 = {2, 1};
+//        System.out.println("The array contains duplicate: " + containsNearbyAlmostDuplicate(test, 1, 2147483647));
+//        System.out.println("The array contains duplicate: " + containsNearbyAlmostDuplicate(test2, 1, 5));
+//        System.out.println("The array contains duplicate: " + containsNearbyAlmostDuplicate(test3, 1, 1));
+//        System.out.println("The array contains duplicate: " + containsNearbyAlmostDuplicate(new int[]{1, 2}, 0, 1));
+//        System.out.println("The array contains duplicate: " + containsNearbyAlmostDuplicate(new int[]{-1, -1}, 1, 0));
+        System.out.println("The array contains duplicate: " + containsNearbyAlmostDuplicate(new int[]{0, 10, 22, 15, 0, 5, 22, 12, 1, 5}, 3, 3));
     }
+
     public static boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-        Map<Integer, Integer> map = new HashMap<>();
-        int n = nums.length;
-        if (n > 10000) {
-            for (int i = 0; i < n; i++) {
-                if (!map.containsKey(nums[i]))
-                    map.put(nums[i], i);
-                for (int j = 1; j <= t; j++) {
-                    if (map.containsKey(nums[i]+j)) {
-                        int lastPos = map.get(nums[i]+j);
-                        if (i - lastPos <= k) {
-                            return true;
-                        } else {
-                            map.put(nums[i], i);
-                        }
-                    } 
-                    
+        if (nums == null || nums.length == 0) return false;
+        TreeMap<Long, Integer> tree = new TreeMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (tree.size() <= k) {
+                long max = (long) nums[i] + t;
+                long min = (long) nums[i] - t;
+                if (tree.floorKey(max) != null && tree.floorKey(max) >= min) {
+                    return true;
                 }
             }
-        } else {
-        for (int i = 0; i < n; i++) {
-            if (!map.containsKey(nums[i]))
-                map.put(nums[i], i);
-            for (int j = 1; j <= k; j++) {
-                if (i+j < n && Math.abs(nums[i+j]-nums[i]) <= t) {
-                    if ((long) nums[i+j] - (long) nums[i] <= (long) t) {
-                        return true;
-                    }
-                } else {
-                        map.put(nums[i], i);
-                }
-            }
-        }
+            if (tree.size() == k)
+                tree.remove((long) nums[i - k]);
+            tree.put((long) nums[i], i);
         }
         return false;
     }
