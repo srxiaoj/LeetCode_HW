2adbfb3765824dbf7556f137212e8ed425e1178f
Haorui Wu <srxiaoj@gmail.com>
2016-08-11 22:40:57 -0400

    Modified BinaryTreeZigzagLevelOrderTraversal

--- Tree/src/BinaryTreeZigzagLevelOrderTraversal.java
+++ Tree/src/BinaryTreeZigzagLevelOrderTraversal.java
@@ -1,11 +1,9 @@
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Stack;
 
 public class BinaryTreeZigzagLevelOrderTraversal {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
         /**
          *                 6
          *              /    \
@@ -19,7 +17,31 @@ public class BinaryTreeZigzagLevelOrderTraversal {
         printTwoDArrayList(zigzagLevelOrder(root));
     }
 
+    /**
+     * recursive, dfs
+     */
     public static List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> res = new LinkedList<>();
+        helper(res, root, 0);
+        return res;
+    }
+
+    private static void helper(List<List<Integer>> res, TreeNode root, int level) {
+        if (root == null) return;
+        if (res.size() == level) {
+            res.add(new LinkedList<>());
+        }
+
+        if (level % 2 == 0) {
+            res.get(level).add(root.val);
+        } else {
+            res.get(level).add(0, root.val);
+        }
+        helper(res, root.left, level + 1);
+        helper(res, root.right, level + 1);
+    }
+
+  /*  public static List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> intRes = new ArrayList<>();
         if (root == null) return intRes;
         List<Integer> intRoot = new ArrayList<>();
@@ -79,7 +101,7 @@ public class BinaryTreeZigzagLevelOrderTraversal {
             }
         }
         return intRes;
-    }
+    }*/
 
     //print two dimensional array list, which can also be replaced by simply System.out.println(A)
     public static void printTwoDArrayList(List<List<Integer>> A) {
