0532b34fb1e69aabe3877eb149df3dd77d60f0ef
Haorui Wu <srxiaoj@gmail.com>
2016-09-07 14:41:21 -0400

    Modified IntegertoRoman

--- Math/src/IntegertoRoman.java
+++ Math/src/IntegertoRoman.java
@@ -5,6 +5,7 @@ public class IntegertoRoman {
     public static void main(String[] args) {
         IntegertoRoman obj = new IntegertoRoman();
         System.out.println(obj.intToRoman(181));
+        System.out.println(obj.intToRoman(1024));
     }
 
     /**
@@ -16,7 +17,20 @@ public class IntegertoRoman {
         String symbol[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
         int value[] =     {1000, 900, 500, 400, 100,   90,   50,  40,   10,   9,   5,   4,    1};
         StringBuilder sb = new StringBuilder();
-        int start = 0;
+        int i = 0;
+        while (i < value.length && num > 0) {
+            if (num >= value[i]) {
+                int count = num / value[i];
+                for (int j = 0; j < count; j++) {
+                    sb.append(symbol[i]);
+                }
+                num -= count * value[i];
+            }
+            i++;
+        }
+        return sb.toString();
+
+        /*int start = 0;
         while (num > 0) {
             if (start < value.length && num < value[start]) {
                 start++;
@@ -27,6 +41,6 @@ public class IntegertoRoman {
                 num -= num / value[start] * value[start];
             }
         }
-        return sb.toString();
+        return sb.toString();*/
     }
 }
