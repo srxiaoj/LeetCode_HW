923362d60544d27d6c3f07ef5b0465e462b057b3
Haorui Wu <srxiaoj@gmail.com>
2016-09-09 17:01:09 -0400

    Modified Recover Binary Search Tree

--- Tree/src/RecoverBinarySearchTree.java
+++ Tree/src/RecoverBinarySearchTree.java
@@ -10,41 +10,42 @@ public class RecoverBinarySearchTree {
          *           0   4  7    9
          */
         RecoverBinarySearchTree obj = new RecoverBinarySearchTree();
-        TreeNode root = TreeNode.deserializeLevelorder("6,8,2,0,4,7,9");
+//        TreeNode root = TreeNode.deserializeLevelorder("6,8,2,0,4,7,9");
+        TreeNode root = TreeNode.deserializeLevelorder("8,5,25,4,18,7,30,null,null,6,null");
         obj.recoverTree(root);
         TreeNode.printNode(root);
     }
 
-    private TreeNode firstElement = null;
-    private TreeNode secondElement = null;
+    private TreeNode first = null;
+    private TreeNode second = null;
     private TreeNode previous = new TreeNode(Integer.MIN_VALUE);
 
     public void recoverTree(TreeNode root) {
         inorderTraversal(root);
 
         // swap the two values
-        int tmp = firstElement.val;
-        firstElement.val = secondElement.val;
-        secondElement.val = tmp;
+        int tmp = first.val;
+        first.val = second.val;
+        second.val = tmp;
     }
 
     /**
      * 根据 inorder的特性，一定是从小到大进行traversal，错位的两个数，第一个一定是比后一个大，第二个一定是比前一个小
-     * 所以选取两个变量 previous 和 next, 第一个数是当previous > next的时候取previous, 第二个数则是取next
+     * 所以选取两个变量 previous 和 root, 第一个数是当previous > next的时候取previous, 第二个数则是取next
      */
-    public void inorderTraversal(TreeNode next) {
-        if (next == null) return;
-        inorderTraversal(next.left);
+    public void inorderTraversal(TreeNode root) {
+        if (root == null) return;
+        inorderTraversal(root.left);
         // Start of "do some business",
         // If first element has not been found, assign it to prevElement (refer to 6 in the example above)
-        if (firstElement == null && previous.val > next.val) {
-            firstElement = previous; // the first element should be previous
+        if (first == null && previous.val > root.val) {
+            first = previous; // the first element should be previous
         }
-        // If first element is found, assign the second element to the next (refer to 2 in the example above)
-        if (firstElement != null && previous.val > next.val) {
-            secondElement = next; // the second element should be next
+        // If first element is found, assign the second element to the root (refer to 2 in the example above)
+        if (first != null && previous.val > root.val) {
+            second = root; // the second element should be root
         }
-        previous = next;
-        inorderTraversal(next.right);
+        previous = root;
+        inorderTraversal(root.right);
     }
 }
