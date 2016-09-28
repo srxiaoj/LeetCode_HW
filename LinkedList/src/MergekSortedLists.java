4e11e5909b0d32e16520636a3f0f7e4f62756f40
Haorui Wu <srxiaoj@gmail.com>
2016-09-28 00:41:17 -0400

    Modified mergeKLists

--- LinkedList/src/MergekSortedLists.java
+++ LinkedList/src/MergekSortedLists.java
@@ -62,28 +62,28 @@ public class MergekSortedLists {
 
     public ListNode mergeTowLists(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode(0);
-        ListNode copy = dummy;
+        ListNode cur = dummy;
         while (l1 != null && l2 != null) {
             if (l1.val < l2.val) {
-                copy.next = l1;
+                cur.next = l1;
                 l1 = l1.next;
             } else {
-                copy.next = l2;
+                cur.next = l2;
                 l2 = l2.next;
             }
-            copy = copy.next;
+            cur = cur.next;
         }
 
         while (l1 != null) {
-            copy.next = l1;
+            cur.next = l1;
             l1 = l1.next;
-            copy = copy.next;
+            cur = cur.next;
         }
 
         while (l2 != null) {
-            copy.next = l2;
+            cur.next = l2;
             l2 = l2.next;
-            copy = copy.next;
+            cur = cur.next;
         }
         return dummy.next;
     }
