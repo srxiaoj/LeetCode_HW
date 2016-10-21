a3dd062a23e36f0f946ca5bc53c43eafe2c1454a
srxiaoj <srxiaoj@gmail.com>
2016-10-21 16:33:36 -0400

    Add SplitArrayLargestSum

--- Binary Search/src/BinarySearch.java
+++ Binary Search/src/BinarySearch.java
@@ -5,15 +5,16 @@
  */
 public class BinarySearch {
     public static void main(String[] args) {
-        int[] test = new int[] {1,2,3,4,5,6};
+        int[] test = new int[]{1, 2, 3, 4, 5, 6};
         int target = 8;
-        System.out.println(BinarySearch(test, target, 0, test.length-1));
+        System.out.println(BinarySearch(test, target, 0, test.length - 1));
     }
+
     public static int BinarySearch(int[] array, int k, int l, int r) {
-        
+
         while (l <= r) {// <= is essential, for case like there is only one element
-            int mid = (l+r)/2;
-            if (array[mid] == k){
+            int mid = (l + r) / 2;
+            if (array[mid] == k) {
                 return mid;
             } else if (array[mid] > k) {
                 r = mid - 1;
@@ -21,7 +22,7 @@ public class BinarySearch {
                 l = mid + 1;
             }
         }
-        if (k > array[array.length-1])
+        if (k > array[array.length - 1])
             return l;
         return r;//if none is found
     }
--- Dynamic Programming/src/SplitArrayLargestSum.java
+++ Dynamic Programming/src/SplitArrayLargestSum.java
@@ -5,32 +5,58 @@ public class SplitArrayLargestSum {
     public static void main(String[] args) {
         System.out.println(splitArray(new int[]{7, 2, 5, 10, 8}, 2));
         System.out.println(splitArray(new int[]{7, 2, 5, 10, 8}, 3));
+        System.out.println(splitArray(new int[]{1, 2147483647}, 2));
+        System.out.println(splitArray(new int[]{1, 4, 4}, 3));
     }
 
+    /**
+     * O(n log(sum-max))
+     */
     public static int splitArray(int[] nums, int m) {
-        int count = m - 1, used = 0;
-        int max = nums[0];
-        int n = nums.length;
-        int[] sum = new int[n];
-        sum[0] = nums[0];
-        for (int i = 1; i < n; i++) {
-            sum[i] = sum[i - 1] + nums[i];
+        int max = 0;
+        long sum = 0;
+        for (int num : nums) {
+            max = Math.max(num, max);
+            sum += num;
         }
+        if (m == 1) return (int) sum;
 
-        int i = 1;
-        while (i < n - 1 && count > 0) {
-            int restAverage = (sum[n - 1] - sum[i - 1]) / count;
-            if (restAverage > sum[i] && (sum[n - 1] - sum[i]) / count < sum[i + 1]) {
-                i++;
+        //binary search
+        long l = max;
+        long r = sum;
+        while (l + 1 < r) {
+            long mid = l + (r - l) / 2;
+            if (valid(mid, nums, m)) {
+                r = mid;
             } else {
-                max = Math.max(max, sum[i] - used);
-                used = sum[i];
-                i++;
-                count--;
+                l = mid;
             }
         }
-//        max = Math.max(max, sum[n - 1] - used);
-        return max;
+        if (valid(l, nums, m)) {
+            return (int) l;
+        } else {
+            return (int) r;
+        }
+    }
+
+    /**
+     * validate whether target is a valid solution or not
+     * i.e. sum of each split should be <= target and number of splits should be < m
+     */
+    public static boolean valid(long target, int[] nums, int m) {
+        int count = 1;
+        long total = 0;
+        for (int num : nums) {
+            total += num;
+            if (total > target) {
+                total = num;
+                count++;
+                if (count > m) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     //print array
