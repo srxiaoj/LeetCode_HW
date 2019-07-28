6a71eeb9dce9a4c9aaed444a2e16a48ebe29b78f
srxiaoj <srxiaoj@gmail.com>
2019-07-28 16:10:23 -0700

    add recursive way to swapnode

--- Backtracking/src/GenerateParentheses.java
+++ Backtracking/src/GenerateParentheses.java
@@ -5,39 +5,34 @@ import java.util.List;
  * Created by thanksgiving on 4/25/16.
  */
 public class GenerateParentheses {
-    public static void main(String[] args) {
-        GenerateParentheses obj = new GenerateParentheses();
-        System.out.println(obj.generateParenthesis(3));
+
+  public static void main(String[] args) {
+    GenerateParentheses obj = new GenerateParentheses();
+    System.out.println(obj.generateParenthesis(3));
+  }
+
+  /**
+   * 优先填左括号至等于n, 然后去填)至右括号数量等于左括号数 ((())) (()()) (())() ()(()) ()()()
+   */
+  public List<String> generateParenthesis(int n) {
+    List<String> list = new ArrayList<String>();
+    helper(list, "", 0, 0, n);
+    return list;
+  }
+
+  public void helper(List<String> list, String str, int left, int right, int max) {
+    if (str.length() == 2 * max) {
+      System.out.println(str);
+      list.add(str);
+      return;
     }
 
-    /**
-     * 优先填左括号至等于n, 然后去填)至右括号数量等于左括号数
-     * ((()))
-     * (()())
-     * (())()
-     * ()(())
-     * ()()()
-     * @param n
-     * @return
-     */
-    public List<String> generateParenthesis(int n) {
-        List<String> list = new ArrayList<String>();
-        helper(list, "", 0, 0, n);
-        return list;
+    // 注意是 if 不是 for loop
+    if (left < max) {
+      helper(list, str + "(", left + 1, right, max);
     }
-
-    public void helper(List<String> list, String str, int left, int right, int max) {
-        if (str.length() == 2 * max) {
-            list.add(str);
-            return;
-        }
-
-        // 注意是 if 不是 for loop
-        if (left < max) {
-            helper(list, str + "(", left + 1, right, max);
-        }
-        if (right < left) {
-            helper(list, str + ")", left, right + 1, max);
-        }
+    if (right < left) {
+      helper(list, str + ")", left, right + 1, max);
     }
+  }
 }
--- LinkedList/src/SwapNodesinPairs.java
+++ LinkedList/src/SwapNodesinPairs.java
@@ -2,27 +2,55 @@
  * Created by thanksgiving on 2/15/16.
  */
 public class SwapNodesinPairs {
-    public static void main(String[] args) {
-        int[] list = {1, 2, 3};
-        ListNode head = ListNode.create(list);
-        SwapNodesinPairs obj = new SwapNodesinPairs();
-        ListNode.printListNode(obj.swapPairs(head));
-    }
 
-    public ListNode swapPairs(ListNode head) {
-        if (head == null || head.next == null) return head;
-        ListNode res = new ListNode(0);
-        res.next = head;
-        ListNode pre = res;
-        ListNode cur = head;
-        while (cur != null && cur.next != null) {
-            ListNode nxt = cur.next;
-            cur.next = nxt.next;
-            nxt.next = cur;
-            pre.next = nxt;
-            pre = cur;
-            cur = cur.next;
-        }
-        return res.next;
+  public static void main(String[] args) {
+    int[] list = {1, 2, 3, 4};
+    ListNode head = ListNode.create(list);
+    SwapNodesinPairs obj = new SwapNodesinPairs();
+    ListNode.printListNode(obj.swapPairs(head));
+  }
+
+  public ListNode swapPairs(ListNode head) {
+    if (head == null || head.next == null) {
+      return head;
     }
+    ListNode res = new ListNode(0);
+    res.next = head;
+    ListNode pre = res;
+    ListNode cur = head;
+    while (cur != null && cur.next != null) {
+      ListNode nxt = cur.next;
+      cur.next = nxt.next;
+      nxt.next = cur;
+      pre.next = nxt;
+      pre = cur;
+      cur = cur.next;
+      ListNode.printListNode(cur);
+    }
+    return res.next;
+  }
+
+  /**
+   * recursive
+   * @param head
+   * @return
+   */
+  // public ListNode swapPairs(ListNode head) {
+  //   if (head == null) {
+  //     return null;
+  //   }
+  //   if (head.next == null) {
+  //     return head;
+  //   }
+  //   ListNode next = head.next;
+  //   ListNode nextToNext = head.next.next;
+  //   ListNode dummy = new ListNode(0);
+  //   ListNode res = dummy;
+  //   dummy.next = head.next;
+  //   dummy = dummy.next;
+  //   dummy.next = head;
+  //   dummy = dummy.next;
+  //   dummy.next = swapPairs(nextToNext);
+  //   return res.next;
+  // }
 }
