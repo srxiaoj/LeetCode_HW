65f07465b2e061e92faf55af1d8f9fe2a8bf79e9
srxiaoj <srxiaoj@gmail.com>
2019-08-09 16:02:02 -0700

    format grayCode

--- Backtracking/src/GrayCode.java
+++ Backtracking/src/GrayCode.java
@@ -5,26 +5,54 @@ import java.util.List;
  * Created by thanksgiving on 4/26/16.
  */
 public class GrayCode {
-    public static void main(String[] args) {
-        GrayCode obj = new GrayCode();
-        System.out.println(obj.grayCode(3));
 
+  public static void main(String[] args) {
+    GrayCode obj = new GrayCode();
+    System.out.println(obj.grayCode(2));
+
+  }
+
+  /*public List<Integer> grayCode(int n) {
+    List<Integer> res = new ArrayList<>();
+    dfs(res, new ArrayList<>(), 0, n);
+    return res;
+  }
+
+  private void dfs(List<Integer> res, List<Character> part, int index, int n) {
+    if (part.size() == n) {
+      res.add(bitToInt(part));
+      return;
     }
 
-    /**
-     * 非常巧妙的解法：i ^ (i >> 1)
-     *
-     * @param n
-     * @return
-     */
-    public List<Integer> grayCode(int n) {
-        int count = (int) Math.pow(2, n);
-        List<Integer> res = new ArrayList<>();
-        for (int i = 0; i < count; i++) {
-            int temp = i >> 1;
-            System.out.print(temp + " ");
-            res.add(i ^ temp);
-        }
-        return res;
+    for (int i = 0; i < part.size(); i++) {
+      List<Character> newPart = new ArrayList<>(part);
+      newPart.add(i, (char) index);
+      dfs(res, newPart, index + 1, n);
     }
+  }
+
+  private int bitToInt(List<Character> s) {
+    int res = 0;
+    for (int i = s.size() - 1; i >= 0; i--) {
+      res += Math.pow(2, s.get(i) - '0');
+    }
+    return res;
+  }*/
+
+  /**
+   * 非常巧妙的解法：i ^ (i >> 1)
+   *
+   * @param n
+   * @return
+   */
+  public List<Integer> grayCode(int n) {
+      int count = (int) Math.pow(2, n);
+      List<Integer> res = new ArrayList<>();
+      for (int i = 0; i < count; i++) {
+          int temp = i >> 1;
+          System.out.print(temp + " ");
+          res.add(i ^ temp);
+      }
+      return res;
+  }
 }
