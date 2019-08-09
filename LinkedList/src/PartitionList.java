747bc685ef10600a56e5065985a0fd2d042ffad2
srxiaoj <srxiaoj@gmail.com>
2019-08-08 19:36:21 -0700

    add second solution for partition list

--- LinkedList/src/PartitionList.java
+++ LinkedList/src/PartitionList.java
@@ -1,43 +1,72 @@
 
 public class PartitionList {
-    public static void main(String[] args) {
-        PartitionList obj = new PartitionList();
+
+  public static void main(String[] args) {
+    PartitionList obj = new PartitionList();
 //        ListNode test = ListNode.create(new int[]{2, 1});
 //        ListNode.printListNode(obj.partition(test, 2));
-        ListNode test2 = ListNode.create(new int[]{1, 4, 2, 5, 3, 2});
-        ListNode.printListNode(obj.partition(test2, 3));
-    }
+    ListNode test2 = ListNode.create(new int[]{1, 4, 2, 5, 3, 2});
+    ListNode.printListNode(obj.partition(test2, 3));
+  }
 
-    /**
-     * 找到第一个比 x 大的node，然后进行swap
-     */
-    public ListNode partition(ListNode head, int x) {
-        if (head == null || head.next == null) return head;
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-
-        ListNode cur = dummy.next;
-        ListNode pre = dummy;
-        while (cur != null && cur.val < x) {
-            pre = cur;
-            cur = cur.next;
-        }
-        if (cur == null) return dummy.next;
-        ListNode pivot = cur;
-        ListNode preCur = cur;
-        while (cur != null) {
-            if (cur.val >= x) {
-                preCur = cur;
-                cur = cur.next;
-            } else {
-                ListNode nxt = cur.next;
-                preCur.next = nxt;
-                cur.next = pivot;
-                pre.next = cur;
-                pre = pre.next;
-                cur = nxt;
-            }
-        }
-        return dummy.next;
+  /**
+   * 找到第一个比 x 大的node，然后进行swap
+   */
+  /*public ListNode partition(ListNode head, int x) {
+    if (head == null || head.next == null) {
+      return head;
     }
+    ListNode dummy = new ListNode(0);
+    dummy.next = head;
+
+    ListNode cur = dummy.next;
+    ListNode pre = dummy;
+    while (cur != null && cur.val < x) {
+      pre = cur;
+      cur = cur.next;
+    }
+    if (cur == null) {
+      return dummy.next;
+    }
+    ListNode pivot = cur;
+    ListNode preCur = cur;
+    while (cur != null) {
+      if (cur.val >= x) {
+        preCur = cur;
+        cur = cur.next;
+      } else {
+        ListNode nxt = cur.next;
+        preCur.next = nxt;
+        cur.next = pivot;
+        pre.next = cur;
+        pre = pre.next;
+        cur = nxt;
+      }
+    }
+    return dummy.next;
+  }*/
+
+  public ListNode partition(ListNode head, int x) {
+    ListNode first = new ListNode(0);
+    ListNode res = first;
+
+    ListNode second = new ListNode(0);
+    ListNode secondHead = second;
+    ListNode cur = head;
+
+    while (cur != null) {
+      if (cur.val < x) {
+        first.next = cur;
+        first = first.next;
+      } else {
+        second.next = cur;
+        second = second.next;
+      }
+      cur = cur.next;
+    }
+    second.next = null;
+
+    first.next = secondHead.next;
+    return res.next;
+  }
 }
