745b38763c448f055abc07334386564f99fe7c4c
srxiaoj <srxiaoj@gmail.com>
2019-08-18 14:15:08 -0700

    modify WordBreakII

--- Backtracking/src/WordBreakII.java
+++ Backtracking/src/WordBreakII.java
@@ -4,22 +4,46 @@ import java.util.*;
  * Created by thanksgiving on 5/21/16.
  */
 public class WordBreakII {
-    public static void main(String[] args) {
-        WordBreakII obj = new WordBreakII();
-        Set<String> set1 = new HashSet<>();
-        set1.addAll(Arrays.asList("cat", "cats", "and", "sand", "dog"));
-        System.out.println(obj.wordBreak("catsanddog", set1));
+
+  public static void main(String[] args) {
+    WordBreakII obj = new WordBreakII();
+    Set<String> set1 = new HashSet<>();
+    set1.addAll(Arrays.asList("cat", "cats", "and", "sand", "dog"));
+    System.out.println(obj.wordBreak("catsanddog", set1));
 //        Set<String> set2 = new HashSet<>();
 //        set2.addAll(Arrays.asList("a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"));
 //        System.out.println(obj.wordBreak("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
 //                "aaaaaaaaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", set2));
+  }
+
+  static HashMap<String, LinkedList<String>> map = new HashMap<String, LinkedList<String>>();
+
+  public static List<String> wordBreak(String s, Set<String> wordDict) {
+    if (map.containsKey(s)) {
+      return map.get(s);
     }
 
-    /**
-     * 先用wordBreakI判断是不是有解，有的话再dfs去找剩下的单词
-     * 注意一定要判断单词是否在wordDict中
-     */
-    public List<String> wordBreak(String s, Set<String> wordDict) {
+    LinkedList<String> res = new LinkedList<String>();
+    if (s.length() == 0) {
+      res.add("");
+      return res;
+    }
+    for (String word : wordDict) {
+      if (s.startsWith(word)) {
+        List<String> sublist = wordBreak(s.substring(word.length()), wordDict);
+        for (String sub : sublist) {
+          res.add(word + (sub.isEmpty() ? "" : " ") + sub);
+        }
+      }
+    }
+    map.put(s, res);
+    return res;
+  }
+
+  /**
+   * 先用wordBreakI判断是不是有解，有的话再dfs去找剩下的单词 注意一定要判断单词是否在wordDict中
+   */
+    /*public List<String> wordBreak(String s, Set<String> wordDict) {
         int n = s.length();
         List<String> res = new ArrayList<>();
         if (n == 0) return res;
@@ -39,33 +63,23 @@ public class WordBreakII {
         if (dp[n])
             dfs(res, "", s, wordDict, 0);
         return res;
+    }*/
+  private void dfs(List<String> res, String part, String s, Set<String> set, int pos) {
+    if (pos > s.length()) {
+      return;
+    }
+    if (pos == s.length()) {
+      // 去掉第一个空格
+      part = part.substring(1);
+      res.add(part);
+      return;
     }
 
-    private void dfs(List<String> res, String part, String s, Set<String> set, int pos) {
-        if (pos > s.length()) return;
-        if (pos == s.length()) {
-            // 去掉第一个空格
-            part = part.substring(1);
-            res.add(part);
-            return;
-        }
-
-        for (int i = pos; i < s.length(); i++) {
-            if (set.contains(s.substring(pos, i + 1))) {
-                String newPart = part + " " + s.substring(pos, i + 1);
-                dfs(res, newPart, s, set, i + 1);
-            }
-        }
+    for (int i = pos; i < s.length(); i++) {
+      if (set.contains(s.substring(pos, i + 1))) {
+        String newPart = part + " " + s.substring(pos, i + 1);
+        dfs(res, newPart, s, set, i + 1);
+      }
     }
-
-    public static void printArray(boolean[] A) {
-        System.out.print("[");
-        for (int i = 0; i < A.length; i++) {
-            if (i != A.length - 1)
-                System.out.print(A[i] + ", ");
-            else
-                System.out.print(A[i]);
-        }
-        System.out.println("]");
-    }
+  }
 }
