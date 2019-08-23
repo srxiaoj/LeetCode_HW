58cd32d4436c60bf225b1445bf57cfc4ba030b89
srxiaoj <srxiaoj@gmail.com>
2019-08-23 14:56:15 -0700

    format topologicalSort

--- TopologicalSort/src/CourseSchedule.java
+++ TopologicalSort/src/CourseSchedule.java
@@ -7,121 +7,100 @@ import java.util.Queue;
  */
 public class CourseSchedule {
 
-    public static void main(String[] args) {
-        int[][] prerequisites = {{1, 0}, {2, 0}, {3, 1}, {3, 2}};
-        System.out.println(canFinish(4, prerequisites));
+  public static void main(String[] args) {
+    int[][] prerequisites = {{1, 0}, {2, 0}, {3, 1}, {3, 2}};
+    System.out.println(canFinish(4, prerequisites));
+  }
+
+  public static boolean canFinish(int n, int[][] prerequisites) {
+    Graph g = new Graph(n);
+    for (int i = 0; i < prerequisites.length; i++) {
+      g.addEdge(prerequisites[i][0], prerequisites[i][1]);
+    }
+    return g.topologicalSort();
+  }
+
+  public static boolean canFinishBfs(int n, int[][] prerequisites) {
+    int[][] matrix = new int[n][n]; // i -> j
+    int[] indegree = new int[n];
+
+    for (int i = 0; i < prerequisites.length; i++) {
+      int ready = prerequisites[i][0];
+      int pre = prerequisites[i][1];
+      if (matrix[pre][ready] == 0) {
+        indegree[ready]++; //duplicate case
+      }
+      matrix[pre][ready] = 1;
+    }
+    Utils.printArray(matrix);
+
+    int count = 0;
+    Queue<Integer> queue = new LinkedList();
+    for (int i = 0; i < indegree.length; i++) {
+      if (indegree[i] == 0) {
+        queue.offer(i);
+      }
+    }
+    while (!queue.isEmpty()) {
+      int course = queue.poll();
+      count++;
+      for (int i = 0; i < n; i++) {
+        if (matrix[course][i] != 0) {
+          if (--indegree[i] == 0) {
+            queue.offer(i);
+          }
+        }
+      }
+    }
+    return count == n;
+  }
+
+
+  static class Graph {
+
+    int V;
+    LinkedList<Integer>[] adj;
+
+    public Graph(int v) {
+      this.V = v;
+      adj = new LinkedList[V];
+      for (int i = 0; i < V; i++) {
+        adj[i] = new LinkedList<>();
+      }
     }
 
-    public static boolean canFinish(int n, int[][] prerequisites) {
-        Graph g = new Graph(n);
-        for (int i = 0; i < prerequisites.length; i++) {
-            g.addEdge(prerequisites[i][0], prerequisites[i][1]);
-        }
-        return g.topologicalSort();
+    public void addEdge(int v, int u) {
+      adj[v].add(u);
     }
 
-    public static boolean canFinishBfs(int n, int[][] prerequisites) {
-        int[][] matrix = new int[n][n]; // i -> j
-        int[] indegree = new int[n];
-
-        for (int i = 0; i < prerequisites.length; i++) {
-            int ready = prerequisites[i][0];
-            int pre = prerequisites[i][1];
-            if (matrix[pre][ready] == 0)
-                indegree[ready]++; //duplicate case
-            matrix[pre][ready] = 1;
+    // if true, doesn't contain cycle
+    public boolean topologicalSort() {
+      boolean[] visit = new boolean[V];
+      boolean[] alreadyFinished = new boolean[V];
+      for (int i = 0; i < V; i++) {
+        if (!dfs(visit, alreadyFinished, i)) {
+          return false;
         }
-        printArray(matrix);
-
-        int count = 0;
-        Queue<Integer> queue = new LinkedList();
-        for (int i = 0; i < indegree.length; i++) {
-            if (indegree[i] == 0) queue.offer(i);
-        }
-        while (!queue.isEmpty()) {
-            int course = queue.poll();
-            count++;
-            for (int i = 0; i < n; i++) {
-                if (matrix[course][i] != 0) {
-                    if (--indegree[i] == 0)
-                        queue.offer(i);
-                }
-            }
-        }
-        return count == n;
+      }
+      return true;
     }
 
-
-    static class Graph {
-        int V;
-        LinkedList<Integer>[] adj;
-
-        public Graph(int v) {
-            this.V = v;
-            adj = new LinkedList[V];
-            for (int i = 0; i < V; i++) {
-                adj[i] = new LinkedList<>();
-            }
+    private boolean dfs(boolean[] visit, boolean[] alreadyFinished, int last) {
+      if (visit[last]) {
+        return true;
+      }
+      visit[last] = true;
+      List<Integer> nextList = adj[last];
+      for (int next : nextList) {
+        if (visit[next] && !alreadyFinished[next]) {
+          return false;
         }
-
-        public void addEdge(int v, int u) {
-            adj[v].add(u);
+        if (!dfs(visit, alreadyFinished, next)) {
+          return false;
         }
-
-        public boolean topologicalSort() {
-            boolean[] visit = new boolean[V];
-            boolean[] alreadyFinished = new boolean[V];
-            for (int i = 0; i < V; i++) {
-                if (!dfs(visit, alreadyFinished, i)) {
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        private boolean dfs(boolean[] visit, boolean[] alreadyFinished, int last) {
-            if (visit[last]) return true;
-            visit[last] = true;
-            List<Integer> nextList = adj[last];
-            for (int next : nextList) {
-                if (visit[next] && !alreadyFinished[next]) {
-                    return false;
-                }
-                if (!dfs(visit, alreadyFinished, next)) {
-                    return false;
-                }
-            }
-            alreadyFinished[last] = true;
-            return true;
-        }
+      }
+      alreadyFinished[last] = true;
+      return true;
     }
-
-    //print array
-    public static void printArray(int[] A) {
-        System.out.print("[");
-        for (int i = 0; i < A.length; i++) {
-            if (i != A.length - 1)
-                System.out.print(A[i] + ", ");
-            else
-                System.out.print(A[i]);
-        }
-        System.out.println("]");
-    }
-
-    public static void printArray(int[][] A) {
-        for (int i = 0; i < A.length; i++) {
-            System.out.print("[");
-            for (int j = 0; j < A[i].length; j++) {
-                if (j != A[i].length - 1) {
-                    System.out.print(A[i][j] + ", ");
-                } else
-                    System.out.print(A[i][j]);
-
-            }
-            System.out.println("]");
-        }
-        System.out.println("");
-    }
-
-
+  }
 }
--- TopologicalSort/src/CourseScheduleII.java
+++ TopologicalSort/src/CourseScheduleII.java
@@ -6,78 +6,74 @@ import java.util.Stack;
  * Created by thanksgiving on 7/26/16.
  */
 public class CourseScheduleII {
-    public static void main(String[] args) {
-        int[][] prerequisites = {{1, 0}, {2, 0}, {3, 1}, {3, 2}};
-        int[] res = findOrder(4, prerequisites);
-        printArray(res);
+
+  public static void main(String[] args) {
+    int[][] prerequisites = {{1, 0}, {2, 0}, {3, 1}, {3, 2}};
+    int[] res = findOrder(4, prerequisites);
+    Utils.printArray(res);
+  }
+
+  public static int[] findOrder(int n, int[][] prerequisites) {
+    Graph g = new Graph(n);
+    for (int i = 0; i < prerequisites.length; i++) {
+      g.addEdge(prerequisites[i][0], prerequisites[i][1]);
+    }
+    return g.topologicalSort();
+  }
+
+  static class Graph {
+
+    int V;
+    LinkedList<Integer>[] adj;
+
+    public Graph(int v) {
+      this.V = v;
+      adj = new LinkedList[V];
+      for (int i = 0; i < V; i++) {
+        adj[i] = new LinkedList<>();
+      }
     }
 
-    public static int[] findOrder(int n, int[][] prerequisites) {
-        Graph g = new Graph(n);
-        for (int i = 0; i < prerequisites.length; i++) {
-            g.addEdge(prerequisites[i][0], prerequisites[i][1]);
-        }
-        return g.topologicalSort();
+    public void addEdge(int v, int u) {
+      adj[v].add(u);
     }
 
-    static class Graph {
-        int V;
-        LinkedList<Integer>[] adj;
-        public Graph(int v) {
-            this.V = v;
-            adj = new LinkedList[V];
-            for (int i = 0; i < V; i++) {
-                adj[i] = new LinkedList<>();
-            }
+    public int[] topologicalSort() {
+      int[] res = new int[V];
+      boolean[] visit = new boolean[V];
+      boolean[] alreadyFinished = new boolean[V];
+      Stack<Integer> stack = new Stack<>();
+      for (int i = 0; i < V; i++) {
+        if (!dfs(visit, alreadyFinished, i, stack)) {
+          return new int[0];
         }
-        public void addEdge(int v, int u) {
-            adj[v].add(u);
-        }
-
-        public int[] topologicalSort() {
-            int[] res = new int[V];
-            boolean[] visit = new boolean[V];
-            boolean[] alreadyFinished = new boolean[V];
-            Stack<Integer> stack = new Stack<>();
-            for (int i = 0; i < V; i++) {
-                if (!dfs(visit, alreadyFinished, i, stack)) {
-                    return new int[0];
-                }
-            }
-            int i = V - 1;
-            while (!stack.isEmpty()) {
-                res[i] = stack.pop();
-                i--;
-            }
-            return res;
-        }
-
-        private boolean dfs(boolean[] visit, boolean[] alreadyFinished, int last, Stack<Integer> stack) {
-            if (visit[last]) return true;
-            visit[last] = true;
-            List<Integer> nextList = adj[last];
-            for (int next : nextList) {
-                if (visit[next] && !alreadyFinished[next]) {
-                    return false;
-                }
-                if (!dfs(visit, alreadyFinished, next, stack)) {
-                    return false;
-                }
-            }
-            stack.push(last);
-            alreadyFinished[last] = true;
-            return true;
-        }
+      }
+      int i = V - 1;
+      while (!stack.isEmpty()) {
+        res[i] = stack.pop();
+        i--;
+      }
+      return res;
     }
 
-    public static void printArray(int[] A) {
-        System.out.print("[");
-        for (int i = 0; i < A.length; i++) {
-            if (i != A.length - 1)
-                System.out.print(A[i] + ", ");
-            else
-                System.out.print(A[i]);
+    private boolean dfs(boolean[] visit, boolean[] alreadyFinished, int last,
+        Stack<Integer> stack) {
+      if (visit[last]) {
+        return true;
+      }
+      visit[last] = true;
+      List<Integer> nextList = adj[last];
+      for (int next : nextList) {
+        if (visit[next] && !alreadyFinished[next]) {
+          return false;
         }
-        System.out.println("]");
+        if (!dfs(visit, alreadyFinished, next, stack)) {
+          return false;
+        }
+      }
+      stack.push(last);
+      alreadyFinished[last] = true;
+      return true;
     }
+  }
 }
