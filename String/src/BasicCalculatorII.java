35fd45178e52e27d7754693e283b43b5308aaa5a
srxiaoj <srxiaoj@gmail.com>
2019-08-25 20:17:16 -0700

    modify calculatorII

--- String/src/BasicCalculatorII.java
+++ String/src/BasicCalculatorII.java
@@ -13,32 +13,28 @@ public class BasicCalculatorII {
 
     public static int calculate(String s) {
         if (s == null || s.length() == 0) return 0;
-        int len = s.length();
-        Stack<Integer> stack = new Stack<Integer>();
         int num = 0;
+        Stack<Integer> stack = new Stack<>();
         char sign = '+';
-        for (int i = 0; i < len; i++) {
-            if (Character.isDigit(s.charAt(i))) {
-                num = num * 10 + s.charAt(i) - '0';
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                num = num * 10 + c - '0';
             }
-            if ((!Character.isDigit(s.charAt(i)) && s.charAt(i) != ' ') || i == len - 1) {
+            if (!Character.isDigit(c) && c != ' ' || i == s.length() - 1) {
                 if (sign == '-') {
                     stack.push(-num);
-                }
-                if (sign == '+') {
+                } else if (sign == '+') {
                     stack.push(num);
+                } else if (sign == '*') {
+                    stack.push(stack.pop() * num);
+                } else if (sign == '/') {
+                    stack.push(stack.pop() / num);
                 }
-                if (sign == '*') {
-                    stack.push(stack.pop() * num);//pop the last element and multiply with current element
-                }
-                if (sign == '/') {
-                    stack.push(stack.pop() / num);//pop the last element and divide with current element
-                }
-                sign = s.charAt(i);
+                sign = c;
                 num = 0;
             }
         }
-
         int re = 0;
         for (int i : stack) {
             re += i;
