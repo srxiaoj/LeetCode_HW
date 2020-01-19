a9d4e0f0b437d51e96c5b0ddc9a15a6b64f4c0e2
srxiaoj <srxiaoj@gmail.com>
2020-01-18 16:58:21 -0800

    add medianOfArray

---
+++ Sort/src/MedianOfArray.java
@@ -0,0 +1,61 @@
+public class MedianOfArray {
+
+  public static void main(String[] args) {
+    int[] nums = new int[]{5, 8, 3, 1, 6};
+    System.out.println(getMedian(nums));
+
+    int[] nums2 = new int[]{13, 5, 8, 10};
+    System.out.println(getMedian(nums2));
+  }
+
+  // 方法一: partition
+  private static int getMedian(int[] nums) {
+    int l = 0, r = nums.length;
+    if (nums.length % 2 == 0) {
+      int left = (l + r) / 2 - 1;
+      int right = (l + r) / 2;
+      return (getKthLargest(nums, left) + getKthLargest(nums, right)) / 2;
+    } else {
+      return getKthLargest(nums, (l + r) / 2);
+    }
+  }
+
+  private static int getKthLargest(int[] nums, int k) {
+    int l = 0, r = nums.length - 1;
+    int mid = 0;
+    while (mid != k) {
+      mid = partition(nums, l, r);
+      if (mid == k) {
+        return nums[mid];
+      }
+      if (mid < k) {
+        l = mid + 1;
+      } else {
+        r = mid - 1;
+      }
+    }
+    return 0;
+  }
+
+  private static int partition(int[] nums, int start, int end) {
+    int p = start;
+    int pVal = nums[start];
+    int l = start, r = end;
+
+    for (int i = start + 1; i <= end; i++) {
+      if (nums[i] > pVal) {
+        p++;
+        swap(nums, i, p);
+      }
+    }
+    swap(nums, p, l);
+    Utils.printArray(nums);
+    return p;
+  }
+
+  private static void swap(int[] array, int a, int b) {
+    int temp = array[a];
+    array[a] = array[b];
+    array[b] = temp;
+  }
+}
