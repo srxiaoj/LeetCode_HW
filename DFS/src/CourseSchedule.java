e037dcbd16d0e3df039d90f8ff552ac2e20197db
srxiaoj <srxiaoj@gmail.com>
2020-01-25 14:47:20 -0800

    modify courseSchedule

--- DFS/src/CourseSchedule.java
+++ DFS/src/CourseSchedule.java
@@ -5,63 +5,70 @@ import java.util.LinkedList;
  * Created by thanksgiving on 12/23/15.
  */
 public class CourseSchedule {
-    public static void main(String[] args) {
-        CourseSchedule obj = new CourseSchedule();
-        int numCourses = 3;
-        int[][] pre = {
-                {1, 0},
-                {2, 0},
-                {1, 2}
-        };
-        System.out.println("can finish: " + obj.canFinish(numCourses, pre));
+
+  public static void main(String[] args) {
+    CourseSchedule obj = new CourseSchedule();
+    int numCourses = 3;
+    int[][] pre = {
+        {1, 0},
+        {2, 0},
+        {1, 2}
+    };
+    System.out.println("can finish: " + obj.canFinish(numCourses, pre));
+  }
+
+  public boolean canFinish(int numCourses, int[][] prerequisites) {
+    Graph g = new Graph(numCourses);
+    for (int i = 0; i < prerequisites.length; i++) {
+      g.addEdge(prerequisites[i][0], prerequisites[i][1]);
+    }
+    return g.topologicalSort();
+  }
+
+  class Graph {
+
+    int v;
+    LinkedList<Integer>[] adj;
+
+    public Graph(int n) {
+      this.v = n;
+      adj = new LinkedList[n];
+      for (int i = 0; i < n; i++) {
+        adj[i] = new LinkedList<>();
+      }
     }
 
-    private LinkedList<Integer>[] adj;
-
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        return !isCyclic(numCourses, prerequisites);
+    private void addEdge(int u, int v) {
+      adj[u].add(v);
     }
 
-    private boolean isCyclic(int numCourses, int[][] prerequisites) {
-        // initialize adjacency list
-        adj = new LinkedList[numCourses];
-        boolean[] visited = new boolean[numCourses];
-        boolean[] recStack = new boolean[numCourses];
-        for (int i = 0; i < numCourses; i++) {
-            adj[i] = new LinkedList<Integer>();
-            visited[i] = false;
-            recStack[i] = false;
+    private boolean topologicalSort() {
+      boolean[] visit = new boolean[v];
+      boolean[] hasFinished = new boolean[v];
+      for (int i = 0; i < v; i++) {
+        if (!dfs(visit, hasFinished, i)) {
+          return false;
         }
-
-        for (int i = 0; i < prerequisites.length; i++) {
-            addEdge(prerequisites[i][0], prerequisites[i][1]);
-        }
-        for (int i = 0; i < numCourses; i++) {
-            if (!visited[i] && isCyclicUntil(i, visited, recStack)) {
-                return true;
-            }
-        }
-        return false;
+      }
+      return true;
     }
 
-    private boolean isCyclicUntil(int u, boolean[] visited, boolean[] recStack) {
-        if (!visited[u]) {
-            visited[u] = true;
-            recStack[u] = true;
-            Iterator<Integer> it = adj[u].iterator();
-            while (it.hasNext()) {
-                int i = it.next();
-                if (!visited[i] && isCyclicUntil(i, visited, recStack)) {
-                    return true;
-                } else if (recStack[i])
-                    return true;
-            }
-        }
-        recStack[u] = false;
-        return false;
-    }
+    private boolean dfs(boolean[] visit, boolean[] hasFinished, int i) {
+      if (visit[i]) {
+        return true;
+      }
+      visit[i] = true;
 
-    private void addEdge(int v, int w) {
-        adj[v].add(w);
+      for (int next : adj[i]) {
+        if (visit[next] && !hasFinished[next]) {
+          return false;
+        }
+        if (!dfs(visit, hasFinished, next)) {
+          return false;
+        }
+      }
+      hasFinished[i] = true;
+      return true;
     }
+  }
 }
