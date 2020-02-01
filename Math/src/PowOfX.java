f4ef13ccf1b6c1750858747d14b0687ae1373fb6
srxiaoj <srxiaoj@gmail.com>
2020-02-01 10:50:05 -0800

    modify myPow

--- Math/src/PowOfX.java
+++ Math/src/PowOfX.java
@@ -2,7 +2,8 @@
  * Created by thanksgiving on 5/12/16.
  */
 public class PowOfX {
-    public static void main(String[] args) {
+
+  public static void main(String[] args) {
        /* System.out.println(myPow(4, -2));
         System.out.println(myPow(1.00000, -2147483648));
         System.out.println(myPow(2.00000, -2147483648));
@@ -11,24 +12,33 @@ public class PowOfX {
         System.out.println(myPow(2, 20000000));
         System.out.println(1.0 / -2147483648);
         System.out.println(1 / -2147483648);*/
-        System.out.println(myPow(2, -4));
+    System.out.println(myPow(2, -4));
+  }
+
+  /**
+   * n 为负数时候用temp * temp / x， 用myPow(x, -n)会IntegerOverFlow
+   */
+  public static double myPow(double x, int n) {
+    if (n == 0) {
+      return 1;
+    }
+    if (n == 1) {
+      return x;
+    }
+    if (n == -1) {
+      return 1 / x;
     }
 
-    /**
-     * n 为负数时候用temp * temp / x， 用myPow(x, -n)会IntegerOverFlow
-     */
-    public static double myPow(double x, int n) {
-        if (n == 0) return 1;
-        double temp = myPow(x, n / 2);
-        if (n % 2 == 0) {
-            return temp * temp;
-        } else {
-            if (n > 0) {
-                return temp * temp * x;
-            } else {
-                // n < 0, get the reciprocal
-                return (temp * temp) / x;
-            }
-        }
+    double half = myPow(x, n / 2);
+
+    if (n % 2 == 0) {
+      return half * half;
+    } else {
+      if (n > 0) {
+        return x * half * half;
+      } else {
+        return (half * half) / x;
+      }
     }
+  }
 }
